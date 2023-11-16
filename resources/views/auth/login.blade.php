2cf25e74644f48bcf64aabfe18b22dd4c485099a
Xpressglobe <tobeodiachi@yahoo.com>
2023-11-16 17:28:54 +0100

    scaffolding with livewire, datatables and installations

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,126 +1,135 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ config('app.name') }} | Login</title>
 
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+@extends('layouts.auth')
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-          crossorigin="anonymous" referrerpolicy="no-referrer" />
+@section('content')
+    <!-- <body data-layout="horizontal"> -->
+    <div class="auth-page">
+        <div class="container-fluid p-0">
+            <div class="row g-0">
+                <div class="col-xxl-3 col-lg-4 col-md-5">
+                    <div class="auth-full-page-content d-flex p-sm-5 p-4">
+                        <div class="w-100">
+                            <div class="d-flex flex-column h-100">
+                                <div class="mb-4 mb-md-5 text-center">
+                                    <a href="{{ url('/')}}" class="d-block auth-logo">
+                                        <img src="{{asset('lineone/images/logo-sm.svg')}}" alt="" height="28"> <span
+                                            class="logo-txt">{{ config('app.name') }}</span>
+                                    </a>
+                                </div>
+                                <div class="auth-content my-auto">
+                                    <div class="text-center">
+                                        <h5 class="mb-0">Welcome Back !</h5>
+                                        <p class="text-muted mt-2">Sign in to continue to {{ config('app.name') }}.</p>
+                                    </div>
+                                    <form class="mt-4 pt-2" method="post" action="{{ url('/login') }}">
+                                        @csrf
+                                        <div class="form-floating form-floating-custom mb-4">
+                                            <input type="text" class="form-control  @error('email') is-invalid @enderror" id="input-email"
+                                                placeholder="Enter User Name" value="{{ old('email') }}" name="email">
+                                            <label for="input-email">Email</label>
+                                            <div class="form-floating-icon">
+                                                <i data-feather="mail"></i>
+                                            </div>
+                                            @error('email')
+                                                <span class="error invalid-feedback">{{ $message }}</span>
+                                            @enderror
+                                        </div>
 
-    <!-- AdminLTE -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css"
-          integrity="sha512-mxrUXSjrxl8vm5GwafxcqTrEwO1/oBNU25l20GODsysHReZo4uhVISzAKzaABH6/tTfAxZrY2FprmeAP5UZY8A=="
-          crossorigin="anonymous" referrerpolicy="no-referrer" />
+                                        <div class="form-floating form-floating-custom mb-4 auth-pass-inputgroup">
+                                            <input type="password"
+                                                class="form-control pe-5 @error('password') is-invalid @enderror"
+                                                id="password-input" placeholder="Enter Password" name="password">
 
-    <!-- iCheck -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css"
-          integrity="sha512-8vq2g5nHE062j3xor4XxPeZiPjmRDh6wlufQlfC6pdQ/9urJkU07NM0tEREeymP++NczacJ/Q59ul+/K2eYvcg=="
-          crossorigin="anonymous"/>
+                                            <button type="button"
+                                                class="btn btn-link position-absolute h-100 end-0 top-0"
+                                                id="password-addon">
+                                                <i class="mdi mdi-eye-outline font-size-18 text-muted"></i>
+                                            </button>
+                                            <label for="input-password">Password</label>
+                                            <div class="form-floating-icon">
+                                                <i data-feather="lock"></i>
+                                            </div>
+                                            @error('password')
+                                                <span class="error invalid-feedback">{{ $message }}</span>
+                                            @enderror
+                                        </div>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+                                        <div class="row mb-4">
+                                            <div class="col">
+                                                <div class="form-check font-size-15">
+                                                    <input class="form-check-input" type="checkbox"
+                                                        id="remember-check">
+                                                    <label class="form-check-label font-size-13" for="remember-check">
+                                                        Remember me
+                                                    </label>
+                                                </div>
+                                            </div>
 
-</head>
-<body class="hold-transition login-page">
-<div class="login-box">
-    <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>{{ config('app.name') }}</b></a>
-    </div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <button class="btn btn-primary w-100 waves-effect waves-light"
+                                                type="submit">Log In</button>
+                                        </div>
+                                    </form>
 
-    <!-- /.login-logo -->
+                                    <div class="mt-4 pt-2 text-center">
+                                        <div class="signin-other-title">
+                                            <h5 class="font-size-14 mb-3 text-muted fw-medium">- Sign in with -</h5>
+                                        </div>
 
-    <!-- /.login-box-body -->
-    <div class="card">
-        <div class="card-body login-card-body">
-            <p class="login-box-msg">Sign in to start your session</p>
+                                        <ul class="list-inline mb-0">
+                                            <li class="list-inline-item">
+                                                <a href="javascript:void()"
+                                                    class="social-list-item bg-primary text-white border-primary">
+                                                    <i class="mdi mdi-facebook"></i>
+                                                </a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a href="javascript:void()"
+                                                    class="social-list-item bg-info text-white border-info">
+                                                    <i class="mdi mdi-twitter"></i>
+                                                </a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a href="javascript:void()"
+                                                    class="social-list-item bg-danger text-white border-danger">
+                                                    <i class="mdi mdi-google"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
 
-            <form method="post" action="{{ url('/login') }}">
-                @csrf
-
-                <div class="input-group mb-3">
-                    <input type="email"
-                           name="email"
-                           value="{{ old('email') }}"
-                           placeholder="Email"
-                           class="form-control @error('email') is-invalid @enderror">
-                    <div class="input-group-append">
-                        <div class="input-group-text"><span class="fas fa-envelope"></span></div>
-                    </div>
-                    @error('email')
-                    <span class="error invalid-feedback">{{ $message }}</span>
-                    @enderror
-                </div>
-
-                <div class="input-group mb-3">
-                    <input type="password"
-                           name="password"
-                           placeholder="Password"
-                           class="form-control @error('password') is-invalid @enderror">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
+                                    <div class="mt-3 text-center">
+                                        <p class="text-muted mb-0"><a href="{{ route('password.request') }}"
+                                                class="text-primary fw-semibold">
+                                                Forgot Password?</a> </p>
+                                    </div>
+                                    <div class="mt-1 text-center">
+                                        <p class="text-muted mb-0">Don't have an account ? <a
+                                                href="{{ route('register') }}" class="text-primary fw-semibold">
+                                                Signup now </a> </p>
+                                    </div>
+                                </div>
+                                <div class="mt-4 mt-md-5 text-center">
+                                    <p class="mb-0">©
+                                        <script>
+                                            document.write(new Date().getFullYear())
+                                        </script> {{ config('app.name') }} . Crafted with <i
+                                            class="mdi mdi-heart text-danger"></i> by Themesbrand
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    @error('password')
-                    <span class="error invalid-feedback">{{ $message }}</span>
-                    @enderror
-
+                    <!-- end auth full page content -->
                 </div>
-
-                <div class="row">
-                    <div class="col-8">
-                        <div class="icheck-primary">
-                            <input type="checkbox" id="remember">
-                            <label for="remember">Remember Me</label>
-                        </div>
-                    </div>
-
-                    <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                    </div>
-
-                </div>
-            </form>
-
-            <p class="mb-1">
-                <a href="{{ route('password.request') }}">I forgot my password</a>
-            </p>
-            <p class="mb-0">
-                <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
-            </p>
+                @include('partials.auth-column')
+            </div>
+            <!-- end row -->
         </div>
-        <!-- /.login-card-body -->
+        <!-- end container fluid -->
     </div>
+@endsection
 
-</div>
-<!-- /.login-box -->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
-        integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
-        crossorigin="anonymous"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
-        crossorigin="anonymous"></script>
-
-<!-- AdminLTE App -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/js/adminlte.min.js"
-        integrity="sha512-AJUWwfMxFuQLv1iPZOTZX0N/jTCIrLxyZjTRKQostNU71MzZTEPHjajSK20Kj1TwJELpP7gl+ShXw5brpnKwEg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-</body>
-</html>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,128 +1,166 @@
-<li class="nav-item">
-    <a href="{{ route('roles.index') }}"
-       class="nav-link {{ Request::is('roles*') ? 'active' : '' }}">
-        <p>Roles</p>
+<li>
+    <a href="{{ url('/home') }}">
+        <i data-feather="home"></i>
+        <span class="badge rounded-pill bg-success-subtle text-success float-end">9+</span>
+        <span data-key="t-dashboard">Dashboard</span>
     </a>
+</li>
+<li class="menu-title" data-key="t-apps">Main</li>
+<li>
+    <a href="{{ route('appointments.index') }}">
+        <i data-feather="message-square"></i>
+        <span data-key="t-chat">Appointments</span>
+    </a>
+</li>
+<li>
+    <a href="javascript: void(0);" class="has-arrow">
+        <i data-feather="shopping-cart"></i>
+        <span data-key="t-ecommerce">Settings</span>
+    </a>
+    <ul class="sub-menu" aria-expanded="false">
+        <li>
+            <a href="{{ route('cities.index') }}" class="nav-link {{ Request::is('cities*') ? 'active' : '' }}"
+                key="t-products">
+                Cities
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('clinicUsers.index') }}"
+                class="nav-link {{ Request::is('clinicUsers*') ? 'active' : '' }}" data-key="t-product-detail">
+                Clinic Users
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('countries.index') }}" class="nav-link {{ Request::is('countries*') ? 'active' : '' }}"
+                data-key="t-orders">
+                Countries
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('countryPhoneCodes.index') }}"
+                class="nav-link {{ Request::is('countryPhoneCodes*') ? 'active' : '' }}" data-key="t-customers">
+                Country Phone Codes
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('currencyLists.index') }}"
+                class="nav-link {{ Request::is('currencyLists*') ? 'active' : '' }}" data-key="t-customers">
+                Currency Lists
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('roles.index') }}" class="nav-link {{ Request::is('roles*') ? 'active' : '' }}"
+                data-key="t-roles">
+                Roles
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('users.index') }}" class="nav-link {{ Request::is('users*') ? 'active' : '' }}"
+                data-key="t-users">
+                Users
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('userStages.index') }}"
+                class="nav-link {{ Request::is('userStages*') ? 'active' : '' }}" data-key="t-userStages">
+                User Stages
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('subServices.index') }}"
+                class="nav-link {{ Request::is('subServices*') ? 'active' : '' }}" data-key="t-subServices">
+                Sub Services
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('states.index') }}" class="nav-link {{ Request::is('states*') ? 'active' : '' }}"
+                data-key="t-states">
+                States
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('stages.index') }}" class="nav-link {{ Request::is('stages*') ? 'active' : '' }}"
+                data-key="t-stages">
+                Stages
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('comments.index') }}" class="nav-link {{ Request::is('comments*') ? 'active' : '' }}"
+                data-key="t-comments">
+                Comments
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('services.index') }}" class="nav-link {{ Request::is('services*') ? 'active' : '' }}"
+                data-key="t-services">
+                Services
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('passwordResets.index') }}"
+                class="nav-link {{ Request::is('passwordResets*') ? 'active' : '' }}" data-key="t-passwordResets">
+                Password Resets
+            </a>
+        </li>
+
+        <li>
+            <a href="{{ route('generalSettings.index') }}"
+                class="nav-link {{ Request::is('generalSettings*') ? 'active' : '' }}" data-key="t-generalSettings">
+                General Settings
+            </a>
+        </li>
+
+    </ul>
 </li>
 
 
-<li class="nav-item">
-    <a href="{{ route('users.index') }}"
-       class="nav-link {{ Request::is('users*') ? 'active' : '' }}">
-        <p>Users</p>
+
+<li>
+    <a href="javascript: void(0);" class="has-arrow">
+        <i data-feather="mail"></i>
+        <span data-key="t-email">Email</span>
+    </a>
+    <ul class="sub-menu" aria-expanded="false">
+        <li><a href="apps-email-inbox.html" data-key="t-inbox">Inbox</a></li>
+        <li><a href="apps-email-read.html" data-key="t-read-email">Read Email</a></li>
+    </ul>
+</li>
+
+<li>
+    <a href="apps-calendar.html">
+        <i data-feather="calendar"></i>
+        <span data-key="t-calendar">Calendar</span>
     </a>
 </li>
 
-
-<li class="nav-item">
-    <a href="{{ route('userStages.index') }}"
-       class="nav-link {{ Request::is('userStages*') ? 'active' : '' }}">
-        <p>User Stages</p>
+<li>
+    <a href="javascript: void(0);" class="has-arrow">
+        <i data-feather="users"></i>
+        <span data-key="t-contacts">Contacts</span>
     </a>
+    <ul class="sub-menu" aria-expanded="false">
+        <li><a href="apps-contacts-grid.html" data-key="t-user-grid">User Grid</a></li>
+        <li><a href="apps-contacts-list.html" data-key="t-user-list">User List</a></li>
+        <li><a href="apps-contacts-profile.html" data-key="t-profile">Profile</a></li>
+    </ul>
 </li>
 
-
-<li class="nav-item">
-    <a href="{{ route('subServices.index') }}"
-       class="nav-link {{ Request::is('subServices*') ? 'active' : '' }}">
-        <p>Sub Services</p>
+<li>
+    <a href="javascript: void(0);" class="has-arrow">
+        <i data-feather="trello"></i>
+        <span data-key="t-tasks">Tasks</span>
     </a>
+    <ul class="sub-menu" aria-expanded="false">
+        <li><a href="tasks-list.html" key="t-task-list">Task List</a></li>
+        <li><a href="tasks-kanban.html" key="t-kanban-board">Kanban Board</a></li>
+        <li><a href="tasks-create.html" key="t-create-task">Create Task</a></li>
+    </ul>
 </li>
-
-
-<li class="nav-item">
-    <a href="{{ route('states.index') }}"
-       class="nav-link {{ Request::is('states*') ? 'active' : '' }}">
-        <p>States</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('stages.index') }}"
-       class="nav-link {{ Request::is('stages*') ? 'active' : '' }}">
-        <p>Stages</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('comments.index') }}"
-       class="nav-link {{ Request::is('comments*') ? 'active' : '' }}">
-        <p>Comments</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('services.index') }}"
-       class="nav-link {{ Request::is('services*') ? 'active' : '' }}">
-        <p>Services</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('passwordResets.index') }}"
-       class="nav-link {{ Request::is('passwordResets*') ? 'active' : '' }}">
-        <p>Password Resets</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('generalSettings.index') }}"
-       class="nav-link {{ Request::is('generalSettings*') ? 'active' : '' }}">
-        <p>General Settings</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('currencyLists.index') }}"
-       class="nav-link {{ Request::is('currencyLists*') ? 'active' : '' }}">
-        <p>Currency Lists</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('countryPhoneCodes.index') }}"
-       class="nav-link {{ Request::is('countryPhoneCodes*') ? 'active' : '' }}">
-        <p>Country Phone Codes</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('countries.index') }}"
-       class="nav-link {{ Request::is('countries*') ? 'active' : '' }}">
-        <p>Countries</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('clinicUsers.index') }}"
-       class="nav-link {{ Request::is('clinicUsers*') ? 'active' : '' }}">
-        <p>Clinic Users</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('cities.index') }}"
-       class="nav-link {{ Request::is('cities*') ? 'active' : '' }}">
-        <p>Cities</p>
-    </a>
-</li>
-
-
-<li class="nav-item">
-    <a href="{{ route('appointments.index') }}"
-       class="nav-link {{ Request::is('appointments*') ? 'active' : '' }}">
-        <p>Appointments</p>
-    </a>
-</li>
-
-

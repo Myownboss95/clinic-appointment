79553a3bc6929bc8fecf0a1e67cfe4c6c0e2754e
timtech-wiz <ajetetimothysamuel@gmail.com>
2023-12-19 21:51:13 +0100

    improve-passord-update

--- public/lineone/js/js/pages/pass-addon.init.js
+++ public/lineone/js/js/pages/pass-addon.init.js
@@ -1,1 +1,5 @@
 document.getElementById("password-addon").addEventListener("click",function(){var e=document.getElementById("password-input");"password"===e.type?e.type="text":e.type="password"});
+
+document.getElementById("current-password-addon").addEventListener("click",function(){var e=document.getElementById("current-password-input");"password"===e.type?e.type="text":e.type="password"});
+
+document.getElementById("confirm-password-addon").addEventListener("click",function(){var e=document.getElementById("confirm-password-input");"password"===e.type?e.type="text":e.type="password"});

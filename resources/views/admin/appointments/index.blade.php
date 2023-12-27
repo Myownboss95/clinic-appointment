4e88585d2c192bfdf5ee433ad558fd9088639673
Xpressglobe <tobeodiachi@yahoo.com>
2023-12-27 08:15:19 +0100

    wip

--- resources/views/admin/appointments/index.blade.php
+++ resources/views/admin/appointments/index.blade.php
@@ -25,7 +25,7 @@
 
         <div class="card">
             <div class="card-body p-0">
-                @include('admin.appointments.table')
+                <livewire:appointments-table/>
 
                 <div class="card-footer clearfix">
                     <div class="float-right">

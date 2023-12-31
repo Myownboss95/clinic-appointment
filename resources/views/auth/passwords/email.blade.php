827a11c2df486d0cefd2eed1623a93724ab252e1
timtech-wiz <ajetetimothysamuel@gmail.com>
2023-12-31 13:01:20 +0100

    completed-forgot-password-page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,95 +1,154 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ config('app.name') }}</title>
+@extends('layouts.auth')
 
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+@section('title', 'Recover password')
+@section('content')
+    <!-- <body data-layout="horizontal"> -->
+        <div class="auth-page">
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+                    <div class="col-xxl-3 col-lg-4 col-md-5">
+                        <div class="auth-full-page-content d-flex p-sm-5 p-4">
+                            <div class="w-100">
+                                <div class="d-flex flex-column h-100">
+                                    <div class="mb-4 mb-md-5 text-center">
+                                        <a href="{{ url('/') }}" class="d-block auth-logo">
+                                            <img src="{{ asset('lineone/images/logo-sm.svg') }}" alt="" height="28">
+                                            <span class="logo-txt">{{ config('app.name') }}</span>
+                                        </a>
+                                    </div>
+                                    <div class="auth-content my-auto">
+                                        <div class="text-center">
+                                            <h5 class="mb-0">Reset Password</h5>
+                                            <p class="text-muted mt-2">Reset Password to Continue.</p>
+                                        </div>
+                                        <div class="alert alert-success text-center my-4" role="alert">
+                                            Enter your Email and instructions will be sent to you!
+                                        </div>
+                                        <form class="mt-4" action="{{route('password.email')}}" method="POST">
+                                            @csrf
+                                            <div class="form-floating form-floating-custom mb-4">
+                                                <input type="email" name="email" class="form-control" id="input-email" placeholder="Enter Email">
+                                                <label for="input-email">Email</label>
+                                                <div class="form-floating-icon">
+                                                    <i data-feather="mail"></i>
+                                                </div>
+                                            </div>
+                                            <div class="mb-3 mt-4">
+                                                <button class="btn btn-primary w-100 waves-effect waves-light" type="submit">Reset</button>
+                                            </div>
+                                        </form>
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-          crossorigin="anonymous" referrerpolicy="no-referrer" />
+                                        <div class="mt-5 text-center">
+                                            <p class="text-muted mb-0">Remember It ?  <a href="{{route('login')}}"
+                                                    class="text-primary fw-semibold"> Sign In </a> </p>
+                                        </div>
+                                    </div>
+                                    <div class="mt-4 mt-md-5 text-center">
+                                        <p class="mb-0">© <script>document.write(new Date().getFullYear())</script> Dason   . Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end auth full page content -->
+                    </div>
+                    <!-- end col -->
+                    <div class="col-xxl-9 col-lg-8 col-md-7">
+                        <div class="auth-bg pt-md-5 p-4 d-flex">
+                            <div class="bg-overlay"></div>
+                            <ul class="bg-bubbles">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <!-- end bubble effect -->
+                            <div class="row justify-content-center align-items-end">
+                                <div class="col-xl-7">
+                                    <div class="p-0 p-sm-4 px-xl-0">
+                                        <div id="reviewcarouselIndicators" class="carousel slide" data-bs-ride="carousel">
+                                            <div class="carousel-indicators auth-carousel carousel-indicators-rounded justify-content-center mb-0">
+                                                <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">
+                                                    <img src="assets/images/users/avatar-1.jpg" class="avatar-md img-fluid rounded-circle d-block" alt="...">
+                                                </button>
+                                                <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="1" aria-label="Slide 2">
+                                                    <img src="assets/images/users/avatar-2.jpg" class="avatar-md img-fluid rounded-circle d-block" alt="...">
+                                                </button>
+                                                <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="2" aria-label="Slide 3">
+                                                    <img src="assets/images/users/avatar-3.jpg" class="avatar-md img-fluid rounded-circle d-block" alt="...">
+                                                </button>
+                                            </div>
+                                            <!-- end carouselIndicators -->
+                                            <div class="carousel-inner">
+                                                <div class="carousel-item active">
+                                                    <div class="testi-contain text-center text-white">
+                                                        <i class="bx bxs-quote-alt-left text-success display-6"></i>
+                                                        <h4 class="mt-4 fw-medium lh-base text-white">“I feel confident
+                                                            imposing change
+                                                            on myself. It's a lot more progressing fun than looking back.
+                                                            That's why
+                                                            I ultricies enim
+                                                            at malesuada nibh diam on tortor neaded to throw curve balls.”
+                                                        </h4>
+                                                        <div class="mt-4 pt-1 pb-5 mb-5">
+                                                            <h5 class="font-size-16 text-white">Richard Drews
+                                                            </h5>
+                                                            <p class="mb-0 text-white-50">Web Designer</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
 
-    <!-- AdminLTE -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/css/adminlte.min.css"
-          integrity="sha512-mxrUXSjrxl8vm5GwafxcqTrEwO1/oBNU25l20GODsysHReZo4uhVISzAKzaABH6/tTfAxZrY2FprmeAP5UZY8A=="
-          crossorigin="anonymous" referrerpolicy="no-referrer" />
+                                                <div class="carousel-item">
+                                                    <div class="testi-contain text-center text-white">
+                                                        <i class="bx bxs-quote-alt-left text-success display-6"></i>
+                                                        <h4 class="mt-4 fw-medium lh-base text-white">“Our task must be to
+                                                            free ourselves by widening our circle of compassion to embrace
+                                                            all living
+                                                            creatures and
+                                                            the whole of quis consectetur nunc sit amet semper justo. nature
+                                                            and its beauty.”</h4>
+                                                        <div class="mt-4 pt-1 pb-5 mb-5">
+                                                            <h5 class="font-size-16 text-white">Rosanna French
+                                                            </h5>
+                                                            <p class="mb-0 text-white-50">Web Developer</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
 
-    <!-- iCheck -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css"
-          integrity="sha512-8vq2g5nHE062j3xor4XxPeZiPjmRDh6wlufQlfC6pdQ/9urJkU07NM0tEREeymP++NczacJ/Q59ul+/K2eYvcg=="
-          crossorigin="anonymous"/>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-<body class="hold-transition login-page">
-<div class="login-box">
-    <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>{{ config('app.name') }}</b></a>
-    </div>
-
-    <!-- /.login-logo -->
-    <div class="card">
-        <div class="card-body login-card-body">
-            <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
-
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
-                </div>
-            @endif
-
-            <form action="{{ route('password.email') }}" method="post">
-                @csrf
-
-                <div class="input-group mb-3">
-                    <input type="email"
-                           name="email"
-                           class="form-control @error('email') is-invalid @enderror"
-                           placeholder="Email">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
+                                                <div class="carousel-item">
+                                                    <div class="testi-contain text-center text-white">
+                                                        <i class="bx bxs-quote-alt-left text-success display-6"></i>
+                                                        <h4 class="mt-4 fw-medium lh-base text-white">“I've learned that
+                                                            people will forget what you said, people will forget what you
+                                                            did,
+                                                            but people will never forget
+                                                            how donec in efficitur lectus, nec lobortis metus you made them
+                                                            feel.”</h4>
+                                                        <div class="mt-4 pt-1 pb-5 mb-5">
+                                                            <h5 class="font-size-16 text-white">Ilse R. Eaton</h5>
+                                                            <p class="mb-0 text-white-50">Manager
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- end carousel-inner -->
+                                        </div>
+                                        <!-- end review carousel -->
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    @error('email')
-                        <span class="error invalid-feedback">{{ $message }}</span>
-                    @enderror
+                    <!-- end col -->
                 </div>
-
-                <div class="row">
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-primary btn-block">Send Password Reset Link</button>
-                    </div>
-                    <!-- /.col -->
-                </div>
-            </form>
-
-            <p class="mt-3 mb-1">
-                <a href="{{ route("login") }}">Login</a>
-            </p>
-            <p class="mb-0">
-                <a href="{{ route("register") }}" class="text-center">Register a new membership</a>
-            </p>
+                <!-- end row -->
+            </div>
+            <!-- end container fluid -->
         </div>
-        <!-- /.login-card-body -->
-    </div>
-</div>
-<!-- /.login-box -->
-
-<!-- AdminLTE App -->
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.1.0/js/adminlte.min.js"
-        integrity="sha512-AJUWwfMxFuQLv1iPZOTZX0N/jTCIrLxyZjTRKQostNU71MzZTEPHjajSK20Kj1TwJELpP7gl+ShXw5brpnKwEg=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
+@endsection

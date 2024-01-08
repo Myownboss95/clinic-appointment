09db0998e4c7d98008d4b024680abaacb206b010
Odiachi Daniel Tobechukwu <tobeodiachi@yahoo.com>
2024-01-08 04:43:15 +0100

    Update CreateUserRequest.php

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -33,7 +33,7 @@ class CreateUserRequest extends FormRequest
         return  [
             'first_name' => 'nullable|string|max:255',
             'last_name' => 'nullable|string|max:255',
-            'email' => ['required','string','max:255', Rule::unique('users')->ignore($this->user)],
+            'email' => ['required','string','max:255', 'unique:users,email',
             'dob' => 'nullable|string',
             'phone_number' => 'required|string',
             'city' => 'nullable|integer',
